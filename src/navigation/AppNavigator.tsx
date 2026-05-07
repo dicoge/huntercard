6694abbaef84f003627eea12cafa87b165d532c5
Dicoge <48748046+dicoge@users.noreply.github.com>
2026-05-07 19:29:24 +0800

    fix: 修正 Drawer 圖標與文字間距

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -46,7 +46,7 @@ function MainDrawer() {
           width: 280,
         },
         drawerLabelStyle: {
-          marginLeft: -20,
+          marginLeft: 10,
           fontSize: 16,
         },
         headerStyle: {
@@ -179,6 +179,7 @@ const styles = StyleSheet.create({
   },
   drawerIcon: {
     fontSize: 20,
+    marginRight: 10,
     opacity: 0.6,
   },
   drawerIconFocused: {
