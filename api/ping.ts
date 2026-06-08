da536599df310722c4b60b22927a0823ac6f109a
GitHub Action <action@github.com>
2026-06-08 18:56:02 +0800

    chore: 移除除錯 endpoints (ping/debug)

--- api/ping.ts
+++
@@ -1,6 +0,0 @@
-// Simplest possible Vercel function
-export const config = { runtime: 'nodejs' };
-
-export default async function handler(req: Request) {
-  return Response.json({ status: 'ok', method: req.method, time: Date.now() });
-}
--- api/ping2.js
+++
@@ -1,4 +0,0 @@
-// Plain JavaScript ping - no TypeScript
-module.exports = async (req, res) => {
-  res.json({ status: 'ok', message: 'Pure JS works!' });
-};
